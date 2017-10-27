ecead5984a27e4ce58a58b8b835ee25c9c1f63e0
Alex Choi <alex.ji.choi@gmail.com>
2017-10-27 09:41:06 -0400

    Gitignore updated

--- brunch010/settings.py
+++ brunch010/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['desolate-eyrie-19534.herokuapp.com/', 'brunch-blog.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
